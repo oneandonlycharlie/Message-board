4f75e05ea61173ef07e4ee66ab8df0a9bc2a0e99
Charlie <160505188+oneandonlycharlie@users.noreply.github.com>
2025-03-16 21:30:56 +0100

    Build: add page flip button and styling

--- client/src/components/board.jsx
+++ client/src/components/board.jsx
@@ -1,18 +1,17 @@
 import "../styles/board.css"
+import { useState } from "react";
 
 function Board({messages}) {
-    const calculateTime = (time)=>{
-        console.log(time)
+    const [startNumber, setStart] = useState(0)
+
+    const formatTime = (time)=>{
         const currentTime = new Date();
         const logTime = new Date(time);
         const timeDiff = (currentTime - logTime);
-        console.log(currentTime)
-        console.log(logTime)
-        console.log(timeDiff)
         if (timeDiff/(1000*60) < 1){
             // posted less than 1 minute ago
             time = "just now"
-        } else if (timeDiff/(1000*60) <= 60){
+        } else if (timeDiff/(1000*60) < 60){
             // posted within 1 hour
             time = +Math.round(timeDiff/(1000*60)) + ' ' + 'minutes ago'
         } else if (timeDiff/(1000*60*60) > 1 && timeDiff/(1000*60*60) <= 24){
@@ -30,35 +29,54 @@ function Board({messages}) {
         }
         return time
     }
+
+    // Select entries for the current page
+    const displayMessages = messages.slice(startNumber,startNumber+5);
+
     // Capitalize user name and message
-    messages.map((message)=>{
+    displayMessages.map((message)=>{
         message.name = message.name.charAt(0).toUpperCase() + message.name.slice(1)
         message.message = message.message.charAt(0).toUpperCase() + message.message.slice(1)
-        message.time = calculateTime(message.time)
+        message.time = formatTime(message.time)
     })
 
+    const hendleLeftClick=()=>{
+        setStart((prev)=>prev-5)
+    }
+
+    const hendleRightClick=()=>{
+        setStart((prev)=>prev+5)
+    }
     return(
         <>
             <h1>"Dear past me ..."</h1>
             <div className="line"></div>
             <div className="content-container">
-                {messages.map((message)=>(
+                {displayMessages.map((message)=>(
                     <Message 
                         key={message.message_id}
                         message={message.message}
                         name={message.name}
                         time={message.time} />
                 ))}
+                <span className="count">Page {1+startNumber/5} / {Math.ceil(messages.length/5)}</span>
+                {startNumber >= 5 && 
+                    <button className="left"
+                            onClick={hendleLeftClick}
+                    ><box-icon class='icon' name='chevron-left' ></box-icon></button>}
+                {startNumber+5 <= messages.length && <button className="right"
+                        onClick={hendleRightClick}
+                ><box-icon class='icon' name='chevron-right'></box-icon></button>}
             </div>
         </>
     )
 }
 
-function Message({key, name, message, time}){
+function Message({name, message, time}){
 
     return(
-        <div key={key} className="entry">
-            <div>
+        <div className="entry">
+            <div >
                 <span className="name">{name}</span>
                 <span className="time">{time}</span>
             </div>
